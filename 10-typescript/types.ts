85cbafa14e3363c5dd8e17a751fe6fe660136e4d
Ivan Olexyn <ivan@olexyn.com>
2020-09-01 04:54:05 +0200

    commit from define

---
+++ 10-typescript/types.ts
@@ -0,0 +1,23 @@
+
+
+let isFoo : boolean = false;
+let decimal : number = 7;
+let color : string = "red";
+
+let list: number[] = [1,2,3];
+let list2 : Array<number> = [1,2,3];
+
+let tuple : [string,number];
+tuple = ["hello", 7];
+// tuple = [1,2]; // Error
+
+enum Color{
+  Red,
+  Green,
+  Blue
+}
+
+
+let notSure : unknown = 3;
+
+let pick : any = 5;
