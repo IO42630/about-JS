88409673390aa11455300c1049c5e5ffeae29169
io42630 <ivan@olexyn.com>
2022-08-27 14:14:47 +0200

    + update tslint

--- angular/src/app/selector-demo/selector-demo.component.ts
+++ angular/src/app/selector-demo/selector-demo.component.ts
@@ -10,3 +10,10 @@ import { Component, OnInit } from '@angular/core';
   `
 })
 export class SelectorDemoComponent {}
+
+/**
+ * Each Component has a selector. There are at least 3 variations:
+ * Vanilla: app-selector-demo > vanilla-selector
+ * Element: app-selector-demo > div:nth-child(2)
+ * Class:   app-selector-demo > div.selector-class
+ */
